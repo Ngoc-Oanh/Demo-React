10312c8289b407dd327048885e59784c57941bdb
ngocoanh <phan.thi.ngoc.oanh@oro.com>
2021-07-12 09:50:40 +0700

    Example module event

---
+++ code/4-module-event/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import NewExpense from './components/NewExpese/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+    const expenses = [
+        {
+            id: "e1",
+            title: "Car Tm",
+            amount: 294.34,
+            date: new Date(2021, 6, 9)
+        },
+        {
+            id: "e2",
+            title: "Toilet Pap",
+            amount: 7.9,
+            date: new Date(2021, 12, 1)
+        },
+        {
+            id: "e3",
+            title: "Kenv",
+            amount: 45.34,
+            date: new Date(2021, 5, 10)
+        },
+        {
+            id: "e4",
+            title: "Mar",
+            amount: 4.34,
+            date: new Date(2021, 1, 12)
+        },
+        {
+            id: "e5",
+            title: "Jay",
+            amount: 94.34,
+            date: new Date(2021, 6, 4)
+        },
+    ];
+
+    const addExpenseHandler = expense => {
+        console.log('In App.js');
+        console.log(expense);
+    };
+
+    // return React.createElement(
+    //     'div',
+    //     {},
+    //     React.createElement('h2', {}, "Let's get started!"),
+    //     React.createElement(Expenses, {items:expenses})
+    // );
+
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses items={expenses} />
+        </div>
+    );
+}
+
+export default App;
